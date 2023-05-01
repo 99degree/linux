ff365828b966769b3ed306d944f9d6b5c3f7a87a
root <root@LAPTOP-VDEV89N4>
2023-05-01 18:48:29 +0800

    Revert "drm: qcom: a618: add a618 init code and fw name by the same a619 procedure."
    This reverts commit 6d71b56cafa97cc69b39e7cce2fc02965dd6675e.

--- drivers/gpu/drm/msm/adreno/adreno_device.c
+++ drivers/gpu/drm/msm/adreno/adreno_device.c
@@ -305,8 +305,11 @@ static const struct adreno_info gpulist[] = {
 		.inactive_period = DRM_MSM_INACTIVE_PERIOD,
 		.quirks = ADRENO_QUIRK_HAS_CACHED_COHERENT,
 		.init = a6xx_gpu_init,
-		.zapfw = "a615_zap.mdt",
-		.hwcg = a615_hwcg,
+		.speedbins = ADRENO_SPEEDBINS(
+			{ 0,   0 },
+			{ 169, 1 },
+			{ 174, 2 },
+		),
 	}, {
 		.machine = "qcom,sm4350",
 		.chip_ids = ADRENO_CHIP_IDS(0x06010900),
@@ -504,10 +507,6 @@ MODULE_FIRMWARE("qcom/a530_zap.b00");
 MODULE_FIRMWARE("qcom/a530_zap.b01");
 MODULE_FIRMWARE("qcom/a530_zap.b02");
 MODULE_FIRMWARE("qcom/a540_gpmu.fw2");
-MODULE_FIRMWARE("qcom/a615_zap.mbt");
-MODULE_FIRMWARE("qcom/a615_zap.b00");
-MODULE_FIRMWARE("qcom/a615_zap.b01");
-MODULE_FIRMWARE("qcom/a615_zap.b02");
 MODULE_FIRMWARE("qcom/a619_gmu.bin");
 MODULE_FIRMWARE("qcom/a630_sqe.fw");
 MODULE_FIRMWARE("qcom/a630_gmu.bin");
@@ -741,6 +740,7 @@ static int adreno_probe(struct platform_device *pdev)
 {
 
 	int ret;
+
 	ret = component_add(&pdev->dev, &a3xx_ops);
 	if (ret)
 		return ret;
