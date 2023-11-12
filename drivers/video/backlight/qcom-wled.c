9217aca3c32377d40fb2a055ec4fea81a42a6a17
root <root@LAPTOP-VDEV89N4>
2023-11-13 01:08:46 +0800

    led: backlight: qcom_wled: [RFT]Two changes to reduce cpu and unbalanced enable irq issue.
    The changeset combined two changes:
    	qcom_wled: Move the enable reg into a tasklet and wait for 50ms to enable irq.
    	qcom_wled: Try to fix the irq_ovp unbalance issue.
    
    Should be fine with code check, need to test real case.

--- drivers/video/backlight/qcom-wled.c
+++ drivers/video/backlight/qcom-wled.c
@@ -2,11 +2,12 @@
 /* Copyright (c) 2015, Sony Mobile Communications, AB.
  */
 
+#include <linux/backlight.h>
 #include <linux/delay.h>
+#include <linux/devm-helpers.h>
 #include <linux/interrupt.h>
 #include <linux/ktime.h>
 #include <linux/kernel.h>
-#include <linux/backlight.h>
 #include <linux/module.h>
 #include <linux/of.h>
 #include <linux/of_address.h>
@@ -290,10 +291,22 @@ static int wled5_set_brightness(struct wled *wled, u16 brightness)
 	return rc;
 }
 
+/*yell*/
 static void wled_ovp_work(struct work_struct *work)
 {
 	struct wled *wled = container_of(work,
 					 struct wled, ovp_work.work);
+
+	disable_irq(wled->ovp_irq);
+
+	/* move it here is due to a known issue of spurious irq storm for 10ms */
+	regmap_update_bits(wled->regmap, wled->ctrl_addr +
+                                WLED3_CTRL_REG_MOD_EN,
+                                WLED3_CTRL_REG_MOD_EN_MASK,
+                                1 << WLED3_CTRL_REG_MOD_EN_SHIFT);
+
+	msleep(50);
+
 	enable_irq(wled->ovp_irq);
 }
 
@@ -304,13 +317,6 @@ static int wled_module_enable(struct wled *wled, int val)
 	if (wled->disabled_by_short)
 		return -ENXIO;
 
-	rc = regmap_update_bits(wled->regmap, wled->ctrl_addr +
-				WLED3_CTRL_REG_MOD_EN,
-				WLED3_CTRL_REG_MOD_EN_MASK,
-				val << WLED3_CTRL_REG_MOD_EN_SHIFT);
-	if (rc < 0)
-		return rc;
-
 	if (wled->ovp_irq > 0) {
 		if (val) {
 			/*
@@ -319,12 +325,17 @@ static int wled_module_enable(struct wled *wled, int val)
 			 * enabling the IRQ for 10ms to ensure that the
 			 * soft start is complete.
 			 */
-			schedule_delayed_work(&wled->ovp_work, HZ / 100);
-		} else {
-			if (!cancel_delayed_work_sync(&wled->ovp_work))
-				disable_irq(wled->ovp_irq);
+			schedule_delayed_work(&wled->ovp_work, 0);
 		}
 	}
+
+	/* the only case is to switch it off. safe to do it here */
+	rc = regmap_update_bits(wled->regmap, wled->ctrl_addr +
+                                WLED3_CTRL_REG_MOD_EN,
+                                WLED3_CTRL_REG_MOD_EN_MASK,
+                                val << WLED3_CTRL_REG_MOD_EN_SHIFT);
+	if (rc < 0)
+		return rc;
 
 	return 0;
 }
@@ -524,7 +535,6 @@ static int wled5_cabc_config(struct wled *wled, bool enable)
 	return 0;
 }
 
-#define WLED_SHORT_DLY_MS			20
 #define WLED_SHORT_CNT_MAX			5
 #define WLED_SHORT_RESET_CNT_DLY_US		USEC_PER_SEC
 
@@ -556,7 +566,7 @@ static irqreturn_t wled_short_irq_handler(int irq, void *_wled)
 
 	wled->last_short_event = ktime_get();
 
-	msleep(WLED_SHORT_DLY_MS);
+	/* no more need to sleep 20ms in irq handler, put it in a tasklet for 50ms */
 	rc = wled_module_enable(wled, true);
 	if (rc < 0)
 		dev_err(wled->dev, "wled enable failed rc:%d\n", rc);
@@ -1596,7 +1606,7 @@ static int wled_configure_ovp_irq(struct wled *wled,
 	}
 
 	rc = devm_request_threaded_irq(wled->dev, wled->ovp_irq, NULL,
-				       wled_ovp_irq_handler, IRQF_ONESHOT,
+				       wled_ovp_irq_handler, IRQF_ONESHOT | IRQ_TYPE_EDGE_RISING,
 				       "wled_ovp_irq", wled);
 	if (rc < 0) {
 		dev_err(wled->dev, "Unable to request ovp_irq (err:%d)\n",
@@ -1605,14 +1615,12 @@ static int wled_configure_ovp_irq(struct wled *wled,
 		return 0;
 	}
 
+	devm_delayed_work_autocancel(wled->dev, &wled->ovp_work, wled_ovp_work);
+
 	rc = regmap_read(wled->regmap, wled->ctrl_addr +
 			 WLED3_CTRL_REG_MOD_EN, &val);
 	if (rc < 0)
 		return rc;
-
-	/* Keep OVP irq disabled until module is enabled */
-	if (!(val & WLED3_CTRL_REG_MOD_EN_MASK))
-		disable_irq(wled->ovp_irq);
 
 	return 0;
 }
@@ -1693,8 +1701,6 @@ static int wled_probe(struct platform_device *pdev)
 		dev_err(wled->dev, "Invalid WLED version\n");
 		break;
 	}
-
-	INIT_DELAYED_WORK(&wled->ovp_work, wled_ovp_work);
 
 	rc = wled_configure_short_irq(wled, pdev);
 	if (rc < 0)
