f32a73eaf2480a01b331e9b0b5f64a5919565efb
root <root@LAPTOP-VDEV89N4>
2023-11-13 03:16:35 +0800

    led: backlight: qcom_wled: Reduce cpu usage from #1 interrupt.
    Need to disable the irq in handler!!! It reduced 90% of cpu.
    
        after:
                   CPU0
        ====       ==== ==============
          9:        855 arch_mem_timer
        ...
        116:        304 18200000.rsc
        128:          0 wled_short_irq
        129:          1 wled_ovp_irq
    
    Move the low-half of irq_handler into tasklet, reduced 70% of cpu.
    
        after:
                   CPU0
        ====       ====  ===================
          9:       5731  arch_mem_timer
        ...
        116:        295  18200000.rsc
        128:          0  wled_short_irq
        129:     364409  wled_ovp_irq

--- drivers/video/backlight/qcom-wled.c
+++ drivers/video/backlight/qcom-wled.c
@@ -201,8 +201,12 @@ struct wled {
 	int short_irq;
 	int ovp_irq;
 
+#define WLED_STATUS_CHECKING_FAULT	BIT(0)
+	int status;	/* store the low-half of irq handler */
+
 	struct wled_config cfg;
 	struct delayed_work ovp_work;
+	struct delayed_work fault_work;
 
 	/* Configures the brightness. Applicable for wled3, wled4 and wled5 */
 	int (*wled_set_brightness)(struct wled *wled, u16 brightness);
@@ -291,7 +295,6 @@ static int wled5_set_brightness(struct wled *wled, u16 brightness)
 	return rc;
 }
 
-/*yell*/
 static void wled_ovp_work(struct work_struct *work)
 {
 	struct wled *wled = container_of(work,
@@ -848,12 +851,14 @@ static irqreturn_t wled_ovp_irq_handler(int irq, void *_wled)
 	int rc;
 	u32 int_sts, fault_sts;
 
+	disable_irq(wled->ovp_irq);
+
 	rc = regmap_read(wled->regmap,
 			 wled->ctrl_addr + WLED3_CTRL_REG_INT_RT_STS, &int_sts);
 	if (rc < 0) {
 		dev_err(wled->dev, "Error in reading WLED3_INT_RT_STS rc=%d\n",
 			rc);
-		return IRQ_HANDLED;
+		goto exit;
 	}
 
 	rc = regmap_read(wled->regmap, wled->ctrl_addr +
@@ -861,7 +866,7 @@ static irqreturn_t wled_ovp_irq_handler(int irq, void *_wled)
 	if (rc < 0) {
 		dev_err(wled->dev, "Error in reading WLED_FAULT_STATUS rc=%d\n",
 			rc);
-		return IRQ_HANDLED;
+		goto exit;
 	}
 
 	if (fault_sts & (WLED3_CTRL_REG_OVP_FAULT_BIT |
@@ -870,14 +875,36 @@ static irqreturn_t wled_ovp_irq_handler(int irq, void *_wled)
 			int_sts, fault_sts);
 
 	if (fault_sts & WLED3_CTRL_REG_OVP_FAULT_BIT) {
-		if (wled->wled_auto_detection_required(wled)) {
-			mutex_lock(&wled->lock);
-			wled_auto_string_detection(wled);
-			mutex_unlock(&wled->lock);
-		}
+		if (wled->wled_auto_detection_required(wled))
+			schedule_delayed_work(&wled->fault_work, 0);
 	}
 
+exit:
+        enable_irq(wled->ovp_irq);
+
 	return IRQ_HANDLED;
+}
+
+static void wled_fault_ovp_work(struct work_struct *work) {
+	struct wled *wled = container_of(work,
+                                         struct wled, fault_work.work);
+
+	mutex_lock(&wled->lock);
+
+	if (!(wled->status & WLED_STATUS_CHECKING_FAULT))
+		wled->status |= WLED_STATUS_CHECKING_FAULT;
+	else
+		goto exit;
+
+	mutex_unlock(&wled->lock);
+
+	wled_auto_string_detection(wled);
+
+	mutex_lock(&wled->lock);
+
+	wled->status &= ~WLED_STATUS_CHECKING_FAULT;
+exit:
+	mutex_unlock(&wled->lock);
 }
 
 static int wled3_setup(struct wled *wled)
@@ -1617,10 +1644,13 @@ static int wled_configure_ovp_irq(struct wled *wled,
 
 	devm_delayed_work_autocancel(wled->dev, &wled->ovp_work, wled_ovp_work);
 
+	devm_delayed_work_autocancel(wled->dev, &wled->fault_work, wled_fault_ovp_work);
+
 	rc = regmap_read(wled->regmap, wled->ctrl_addr +
 			 WLED3_CTRL_REG_MOD_EN, &val);
 	if (rc < 0)
 		return rc;
+
 
 	return 0;
 }
