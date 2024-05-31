ed59017ff1841eccc3212e9860ad977a651e60ac
99degree <49397123+99degree@users.noreply.github.com>
2024-05-31 13:36:30 +0800

    Add files via upload

--- merge.sh
+++ merge.sh
@@ -1,6 +1,7 @@
 # rem
 git fetch origin master
 git checkout origin/master -f
+git branch base-$(date +"%Y%m%d")
 git merge sm7125 --no-edit
 git merge clk --no-edit
 git merge power --no-edit
@@ -15,4 +16,5 @@ git merge misc --no-edit
 git merge sound --no-edit
 git merge pd-mapper --no-edit
 git rebase origin/master
-git branch next-$(date +"%Y%m%d")
+git branch working-$(date +"%Y%m%d")
+git push 99degree HEAD:refs/heads/working-20240530
