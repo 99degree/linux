b59e4ebf53249463e423b28f91101c6aa6600d5b
George Chan <gchan9527@gmail.com>
2025-06-20 22:38:26 +0800

    power: qcom_smbx: Fix a compile issue with missing return
    Signed-off-by: George Chan <gchan9527@gmail.com>

--- drivers/power/supply/qcom_smbx.c
+++ drivers/power/supply/qcom_smbx.c
@@ -579,12 +579,13 @@ static int smb2_get_prop_health(struct smb_chip *chip, int *val)
 
 static int smb_get_prop_health(struct smb_chip *chip, int *val)
 {
-	switch (chip->gen) {
-	case SMB2:
+	if (chip->gen == SMB2)
 		return smb2_get_prop_health(chip, val);
-	case SMB5:
+	else if (chip->gen == SMB5)
 		return smb5_get_prop_health(chip, val);
-	}
+	else
+		pr_warn("chip gen not supported!\n");
+	return -1;
 }
 
 static int smb_get_property(struct power_supply *psy,
@@ -1021,6 +1022,7 @@ static int smb_probe(struct platform_device *pdev)
 	desc = devm_kzalloc(chip->dev, sizeof(smb_psy_desc), GFP_KERNEL);
 	if (!desc)
 		return -ENOMEM;
+
 	memcpy(desc, &smb_psy_desc, sizeof(smb_psy_desc));
 	desc->name =
 		devm_kasprintf(chip->dev, GFP_KERNEL, "%s-charger",
@@ -1066,6 +1068,7 @@ static int smb_probe(struct platform_device *pdev)
 			   smb_handle_usb_icl_change);
 	if (rc < 0)
 		return rc;
+
 	rc = smb_init_irq(chip, &irq, "wdog-bark", smb_handle_wdog_bark);
 	if (rc < 0)
 		return rc;
