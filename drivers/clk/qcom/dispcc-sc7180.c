daba977c30922a88c5eabfff430c0113a9cb0b6a
George Chan <gchan9527@gmail.com>
2025-09-27 01:53:22 +0800

    dispcc-sc7180: Add reset bcr
    Signed-off-by: George Chan <gchan9527@gmail.com>

--- drivers/clk/qcom/dispcc-sc7180.c
+++ drivers/clk/qcom/dispcc-sc7180.c
@@ -17,6 +17,7 @@
 #include "clk-regmap-divider.h"
 #include "common.h"
 #include "gdsc.h"
+#include "reset.h"
 
 enum {
 	P_BI_TCXO,
@@ -168,7 +169,7 @@ static struct clk_rcg2 disp_cc_mdss_byte0_clk_src = {
 		.name = "disp_cc_mdss_byte0_clk_src",
 		.parent_data = disp_cc_parent_data_2,
 		.num_parents = ARRAY_SIZE(disp_cc_parent_data_2),
-		.flags = CLK_SET_RATE_PARENT,
+		.flags = CLK_SET_RATE_PARENT | CLK_OPS_PARENT_ENABLE,
 		.ops = &clk_byte2_ops,
 	},
 };
@@ -640,6 +641,10 @@ static struct gdsc *disp_cc_sc7180_gdscs[] = {
 	[MDSS_GDSC] = &mdss_gdsc,
 };
 
+static const struct qcom_reset_map disp_cc_sc7180_resets[] = {
+	[DISP_CC_MDSS_CORE_BCR] = { 0x34000 },
+};
+
 static struct clk_regmap *disp_cc_sc7180_clocks[] = {
 	[DISP_CC_MDSS_AHB_CLK] = &disp_cc_mdss_ahb_clk.clkr,
 	[DISP_CC_MDSS_AHB_CLK_SRC] = &disp_cc_mdss_ahb_clk_src.clkr,
@@ -689,6 +694,8 @@ static const struct qcom_cc_desc disp_cc_sc7180_desc = {
 	.num_clks = ARRAY_SIZE(disp_cc_sc7180_clocks),
 	.gdscs = disp_cc_sc7180_gdscs,
 	.num_gdscs = ARRAY_SIZE(disp_cc_sc7180_gdscs),
+	.resets = disp_cc_sc7180_resets,
+	.num_resets = ARRAY_SIZE(disp_cc_sc7180_resets),
 };
 
 static const struct of_device_id disp_cc_sc7180_match_table[] = {
